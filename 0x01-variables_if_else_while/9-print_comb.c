d1f15c5ad01f5673b6f22ff261185a9c7c3a7c81
YANOKOTH <okothbrian96@gmail.com>
2023-02-20 03:57:46 -0800

    numbers combination of sinle digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(' ');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
