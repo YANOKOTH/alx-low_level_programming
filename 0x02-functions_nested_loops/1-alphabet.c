a8b5ceab065fccf2d106801bfc5f27f27cb2600a
YANOKOTH <okothbrian96@gmail.com>
2023-02-21 07:57:43 -0800

    main.h file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
  * Return: Always return 0 (Success)
  */
 void print_alphabet(void)
+	void print_alphabet(void)
 {
-	char alpha
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-		_putchar(alpha);
-	}
+	char la;
+
+	for (la  = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
 	_putchar('\n');
 }
