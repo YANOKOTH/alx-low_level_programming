c85cf258ebf99d384a272cbff292a60eea26cb12
YANOKOTH <okothbrian96@gmail.com>
2023-02-24 05:15:20 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers-prints numbers from 0 to 9
+ *_putchar:ch (success)
+ *
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
