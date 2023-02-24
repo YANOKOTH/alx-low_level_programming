b0981b18914cdc7af8f1e53cd0d842b534612f01
YANOKOTH <okothbrian96@gmail.com>
2023-02-24 05:43:24 -0800

    5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints the product of ten and integers
+ * between 0 and 14
+ */
+void more_numbers(void)
+{
+	int c;
+	int h;
+	for (h = 0; h < 10; h++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar((c / 10) + '0');
+			if (c > 9)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
